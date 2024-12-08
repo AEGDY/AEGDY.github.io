7f3fa9ef4cf9a4f48c13917557d941df34df04a8
AEGDY <71035066+AEGDY@users.noreply.github.com>
2024-12-08 23:34:19 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2811611574"
+    serviceWorkerVersion: "2490536286"
   }
 });
